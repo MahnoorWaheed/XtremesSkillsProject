1353dc1dbbc96d1caf52b62217dead034760d010
MahnoorWaheed <mahnoorwaheed8395@gmail.com>
2022-03-21 16:15:57 +0500

    my commit

---
+++ lib/modules/xyz.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Xyz extends StatefulWidget {
+  const Xyz({ Key? key }) : super(key: key);
+
+  @override
+  State<Xyz> createState() => _XyzState();
+}
+
+class _XyzState extends State<Xyz> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
