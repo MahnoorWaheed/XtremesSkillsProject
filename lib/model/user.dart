5a8b342c2abd8b9dc09ea4e5c7508ef7a21615b3
MahnoorWaheed <mahnoorwaheed8395@gmail.com>
2022-04-04 14:34:39 +0500

    4 april

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -5,3 +5,15 @@ class User {
 
   User({this.service = '', this.price = 1, this.description = ''});
 }
+
+
+//  Future<void> getUserByUsername(Map<String, String> map) async {
+//     print(map);
+//     // ignore: deprecated_member_use
+//     return await FirebaseFirestore.instance
+//         .collection('Customer')
+//         .where("Name", isEqualTo: map)
+//         // ignore: deprecated_member_use
+//         .get();
+//   }
+// }
