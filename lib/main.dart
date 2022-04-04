34e1965b7fcc4680ec3de3d116cd300c8a89a8b6
Mahnoor Waheed <mahnoorwaheed@xtremessoft.com>
2022-04-04 09:15:57 +0500

    no changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:xtremes_skills/modules/User%20Screens/first_screen.dart';
-import 'package:xtremes_skills/modules/Worker%20Dashboard/bottom_nav.dart';
-
 import 'package:xtremes_skills/modules/User%20Screens/location.dart';
-import 'package:xtremes_skills/modules/Worker%20Dashboard/location.dart';
-
 import 'package:xtremes_skills/modules/Worker%20Dashboard/skills.dart';
-import 'package:xtremes_skills/modules/auth/screen/signup.dart';
-import 'package:xtremes_skills/modules/login_otp/screen/otp_screen.dart';
-import 'package:xtremes_skills/modules/login_otp/screen/welcome.dart';
-import 'package:xtremes_skills/splashscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 
 Future<void> main() async { 
-  Widget currentpage= const FirstScreen();
+  Widget currentpage= const PickLocation();
 
    WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp();
